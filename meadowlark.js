7473016143ea05fe35eef8afe0f74b9d0d7748b0
Rafael Torraca <rafael.torraca@icloud.com>
2021-08-12 10:24:59 -0300

    doing tests and refactor

--- meadowlark.js
+++ meadowlark.js
@@ -1,44 +1,38 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 
-
 const handlers = require('./lib/handlers');
-
 const app = express();
 
-const PORT = process.env.PORT || 3000;
 
-
-
-// configura o view engine handlebars esse default eh a pagina handlebars main da views/layouts
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
+// engine config
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
+}));
 
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + '/public'));
 
+
+// routes
 app.get('/', handlers.home);
-
 app.get('/about', handlers.about);
-
-app.get('/about/contact', (req, res) => {
-  res.type('text/plain');
-  res.send('About Contact');
-});
-
-app.get('/about/directions', (req, res) => {
-  res.type('text/plain');
-  res.send('About Directions');
-});
-
-// 404 not found personalized
+app.get('/section-test', handlers.sectionTest);
 app.use(handlers.notFound);
-
-// 500 error personalized
 app.use(handlers.serverError);
+
+
+// port / start
+const PORT = process.env.PORT || 3000;
 
 if (require.main === module) {
   app.listen(PORT, () => {
